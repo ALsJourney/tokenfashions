ed900453115c95249de8f3d354460aa553db2312
ALsJourney <aldin.dino@outlook.com>
2023-05-28 09:47:34 +0200

    Fixed button issues.

--- src/components/Socials.tsx
+++ src/components/Socials.tsx
@@ -9,19 +9,29 @@ const Socials: React.FC = () => {
         Really excited to get in touch with us?
       </p>
 
-      <div className=" mt-4 flex flex-col items-center md:flex-row">
+      <div className=" mt-4 flex w-full flex-col items-center justify-between md:flex-row">
         <Link
           href="https://twitter.com/tokenfashions"
           className="mb-4 mr-4 flex items-center"
+          passHref
         >
           <TwitterIcon />
           <span className="ml-2">@tokenfashions</span>
         </Link>
         <Link
           href="https://discord.gg/VByEgNUFdN"
-          className="mb-4 flex items-center rounded bg-white p-4 text-black"
+          className="mb-4 flex items-center"
+          passHref
         >
-          <DiscordIcon /> <span className="ml-2">Join our discord today!</span>
+          <button className="group relative mt-4 h-12 w-64 overflow-hidden rounded-lg bg-white text-black shadow md:mt-0">
+            <div className="absolute inset-0 z-0 w-3 bg-purple-500 transition-all duration-[250ms] ease-out group-hover:w-full"></div>
+            <div className="relative z-10 flex items-center px-6">
+              <DiscordIcon className="group-hover:fill-white" />
+              <span className="ml-2 group-hover:text-white">
+                Join our discord today!
+              </span>
+            </div>
+          </button>
         </Link>
       </div>
     </>
